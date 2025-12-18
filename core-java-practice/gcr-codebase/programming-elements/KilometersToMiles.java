6ca1451b9c78adeb54b1bfd97c782543b6959a90
kapil kumar <kapil.kumar1_cs22@gla.ac.in>
2025-12-18 11:42:37 +0530

    [Kapil Kumar] Aded : program to convert 10.8 km into miles

---
+++ core-java-practice/gcr-codebase/programming-elements/KilometersToMiles.java
@@ -0,0 +1,6 @@
+public class KilometersToMiles {
+    public static void main(String[] args) {
+        double km = 10.8;
+        System.out.println("The Distance "+km+ " km in miles is "+ km*1.6);
+    }
+}
