fb13def4390613eac8f4250094c226f7e1e22fa3
kapil kumar <kapil.kumar1_cs22@gla.ac.in>
2025-12-29 11:48:10 +0530

    [kapil kumar] Add.added new folder as data structure

---
+++ data-structure/LinkedList/EmployeeRecord.java
@@ -0,0 +1,28 @@
+class EmployeeNode {
+    int id;
+    String name;
+    String department;
+    double salary;
+    EmployeeNode next;
+
+    public EmployeeNode(int id, String name, String department, double salary) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+        this.next = null;
+    }
+}
+
+
+
+public class EmployeeRecord {
+    public void deleteBegining(){}
+    public void deleteLast(){}
+    public void deleteSpecificPosition(){}
+    public void addBegining(){}
+    public void addLast(){}
+    public void addSpecificPosition(){}
+    
+
+}
